710729b283f42dc98b2cfcaa3099462499e82c36
王志昱 <wangzhiyu29@jd.com>
2019-11-11 20:09:54 +0800

    refresh gradle settings

--- app/src/main/java/app/itgungnir/kwa/App.kt
+++ app/src/main/java/app/itgungnir/kwa/App.kt
@@ -1,7 +1,5 @@
 package app.itgungnir.kwa
 
-import android.content.Context
-import androidx.multidex.MultiDex
 import androidx.multidex.MultiDexApplication
 import app.itgungnir.kwa.common.util.AppConfig
 
@@ -10,11 +8,6 @@ class App : MultiDexApplication() {
     companion object {
         // 是否是第一次运行项目，通过判断决定是否跳过SplashActivity
         var isFirstRun = true
-    }
-
-    override fun attachBaseContext(base: Context?) {
-        super.attachBaseContext(base)
-        MultiDex.install(this)
     }
 
     override fun onCreate() {
